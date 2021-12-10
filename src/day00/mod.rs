f7705f6e0ed6471f7117b91a0cfea6278230e8bb
Stefan Rupertsberger <s.rupertsberger@gmail.com>
2021-12-10 11:34:09 +0100

    Change template

--- src/day00/mod.rs
+++ src/day00/mod.rs
@@ -1,25 +1,33 @@
-type InputType = u32;
-
-#[aoc_generator(day01)]
-pub fn input_generator(input: &str) -> Vec<InputType> {
-    input.lines()
-        .map(|l| {
-            l.parse()
-        })
-        .collect::<Result<Vec<u32>, std::num::ParseIntError>>()
-        .unwrap()
+#[aoc_generator(day00)]
+pub fn input_generator(input: &str) -> Vec<()> {
+    vec![]
 }
 
-#[aoc(day01, part1)]
-pub fn solve_part1(input: &[InputType]) -> u32 {
+#[aoc(day00, part1)]
+pub fn solve_part1(input: &[()]) -> u32 {
     todo!("Solve part 1");
-
-    panic!("No solution found");
 }
 
-#[aoc(day01, part2)]
-pub fn solve_part2(input: &[InputType]) -> u32 {
+#[aoc(day00, part2)]
+pub fn solve_part2(input: &[()]) -> u32 {
     todo!("Solve part 2");
+}
 
-    panic!("No solution found");
+#[cfg(test)]
+mod test_day00 {
+    use super::{input_generator, solve_part1, solve_part2};
+
+    const INPUT: &str = "";
+
+    #[test]
+    fn part1() {
+        let input = input_generator(INPUT);
+        assert_eq!(solve_part1(&input), 0);
+    }
+
+    #[test]
+    fn part2() {
+        let input = input_generator(INPUT);
+        assert_eq!(solve_part2(&input), 0);
+    }
 }
